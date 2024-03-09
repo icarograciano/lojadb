595940d7b29885845c78e886c37e678d619dbb8f
icarograciano <icaro.graciano@gmail.com>
2024-03-09 12:43:59 -0300

    ajuste update

--- app_atualizar_endereco_e_compras.py
+++ app_atualizar_endereco_e_compras.py
@@ -9,16 +9,6 @@ db = client['lojadb']
 # Criar ou acessar a collection 'vendas'
 vendas_collection = db['vendas']
 
-# Etapa 1: Inserir dados básicos dos clientes
-clientes_data = [
-    {"nome": "João", "vip": 1, "email": "joao@email.com", "telefone": ["9999-1111", "8888-1111"]},
-    {"nome": "Marcos", "vip": 0, "telefone": ["9999-2222"]},
-    {"nome": "Maria", "vip": 1, "email": "maria@email.com", "telefone": ["9999-3333", "8888-3333", "9988-3000"]}
-]
-
-# Inserir documentos na collection
-vendas_collection.insert_many(clientes_data)
-
 # Etapa 2: Atualizar documentos com endereços e dados de compras
 endereco_data = [
     {"nome": "João", "endereco": {"rua": "Rua Um", "numero": 1000, "complemento": "Apto 1 Bloco 1", "cidade": "São Paulo", "estado": "SP"}},
